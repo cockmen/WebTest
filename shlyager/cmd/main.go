9332a6bf7d5950f3eccc98677db37add9741fb4b
kenchik koncheki <kevin.carter1997@gmail.com>
2025-02-07 13:41:01 +0300

    fixes

--- shlyager/cmd/main.go
+++ shlyager/cmd/main.go
@@ -18,14 +18,19 @@ func main() {
 	router := echo.New()
 	api := router.Group("api")
 
+	//Words
 	api.GET("/word/:id", svc.GetWordById)
 	api.POST("/words", svc.CreateWords)
-	api.DELETE("/word/delete/:id", svc.DeleteWord)
-	api.PUT("/word/update/:id", svc.UpdateWord)
+	api.DELETE("/word/:id", svc.DeleteWord)
+	api.PUT("/word/:id", svc.UpdateWord)
+
+	//Reports
 	api.GET("/report/:id", svc.GetReportById)
 	api.POST("/reports", svc.CreateNewReport)
-	api.DELETE("/report/delete/:id", svc.DeleteReport)
-	api.PUT("/report/update/:id", svc.UpdateReport)
+	api.DELETE("/report/:id", svc.DeleteReport)
+	api.PUT("/report/:id", svc.UpdateReport)
+
+	//Search
 	api.GET("/search/ru", svc.SmartSearch)
 
 	router.Logger.Fatal(router.Start(":1323"))
